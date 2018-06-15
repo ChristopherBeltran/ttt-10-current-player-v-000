22f192dce3ed3eb6ed82e973fb964346ab74e7b4
Christopher Beltran <christod.beltran@gmail.com>
2018-06-15 02:05:15 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,10 +2,8 @@
 def turn_count(board)
   turns = 0
   board.each do |space|
-    if space == "X" || space == "O"
+    while space == "X" || space == "O"
       turns += 1
-else 
-  turn_count(board)
   return turns
   end
   end
